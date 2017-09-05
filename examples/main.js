90687cc4cbe0d6baefe7c1f328bd0563c358bba3
Misic <424532913@qq.com>
2017-09-05 18:16:30 +0800

    Add audio effects nodes

--- examples/main.js
+++ examples/main.js
@@ -2,5 +2,7 @@ import Milody from '@/application';
 
 const audio = document.querySelector('#audio');
 let milody = new Milody(audio);
+milody.use('gain');
+milody.gain.gain.value = 1;
 milody.start();
-// console.log(milody);
+console.log(milody);
